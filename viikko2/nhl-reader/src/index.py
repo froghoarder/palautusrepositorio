4c9f06aa63c13e472e34974f77f46419139e3975
Nea <neazahorszky@gmail.com>
2024-11-10 20:40:51 +0200

    Improve playerlist

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,24 +1,18 @@
-import requests
-from player import Player
+#import requests
+from player import Player, PlayerReader, PlayerStats
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
-    response = requests.get(url).json()
+    #response = requests.get(url).json()
 
-    print("JSON-muotoinen vastaus:")
-    print(response)
+    """print("JSON-muotoinen vastaus:")
+    print(response)"""
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality("FIN")
 
-    players = []
-
-    for player_dict in response:
-        if player_dict["nationality"] == "FIN":
-            player = Player(player_dict)
-            players.append(player)
-
-    print("""Players from FIN\n""")
-    
-    for player in sorted(players, key=lambda player: player.points, reverse=True):
-        print(f"{player.name:20}{player.team:6}{player.points}")
+    for player in players:
+        print(player)
 
 if __name__ == "__main__":
     main()
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,6 +1,9 @@
+import requests
+
 class Player:
     def __init__(self, dict):
         self.name = dict['name']
+        self.nationality = dict["nationality"]
         self.team = dict["team"]
         self.goals = dict["goals"]
         self.assists = dict["assists"]
@@ -8,3 +11,20 @@ class Player:
     
     def __str__(self):
         return self.name
+
+class PlayerReader:
+    def __init__(self, url):
+        self.players_dict = requests.get(url).json()
+        self.players_all = [Player(p) for p in self.players_dict]
+
+class PlayerStats():
+    def __init__(self, reader):
+        self.reader = reader
+
+    def top_scorers_by_nationality(self, nationality):
+        top = []
+        for player in self.reader.players_all:
+            if player.nationality == nationality:
+                top.append(player)
+
+        return sorted(top, key=lambda player: player.points, reverse=True)

6d7ffa45dfdbf170ffaac304f1d712c379c4d561
Nea <neazahorszky@gmail.com>
2024-11-10 21:31:20 +0200

    Add functionality

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,4 +1,21 @@
 from player import Player, PlayerReader, PlayerStats
+from rich import print
+from rich.console import Console
+from rich.table import Table
+
+def create_table(players, nationality, season):
+    table = Table(title=f"Top scorers of {nationality} season {season}")
+    table.add_column("name", justify="right", style="cyan", no_wrap=True)
+    table.add_column("team", style="magenta")
+    table.add_column("goals", justify="right", style="green")
+    table.add_column("assists", justify="right", style="green")
+    table.add_column("points", justify="right", style="green")
+
+    for player in players:
+        table.add_row(player.name, player.team, str(player.goals), \
+            str(player.assists), str(player.points))
+            
+    return table
 
 def main():
     
@@ -14,9 +31,9 @@ def main():
         stats = PlayerStats(reader)
         players = stats.top_scorers_by_nationality(nationality)
 
-        for player in players:
-            print(player)
-            print()
+        table = create_table(players, nationality, season)
+        console = Console()
+        console.print(table)
 
 if __name__ == "__main__":
     main()
